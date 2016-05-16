29420cec2ca655782a5aec8342c546e889656c19
chrissoria <csoria1@mail.ccsf.edu>
2016-05-16 00:27:44 -0700

    working menu
    selecting campus will reflect on view

--- CCSF_Resources/MasterViewController.swift
+++ CCSF_Resources/MasterViewController.swift
@@ -10,26 +10,27 @@ import UIKit
 
 class MasterViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    var text: String?
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let text = text {
+            textLabel.text = text
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
